0a6da32668798fcc9c8322cc63f9a71b106486ba
manoj-salunkhe <manojsalunkhe078@gmail.com>
2023-11-21 20:23:31 +0530

    adding signin route

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import cookieSession from "cookie-session";
 import { signupRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    name: "session-test",
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
+
 app.use(signupRouter);
+app.use(signinRouter);
 export { app };
